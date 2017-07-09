c7b9670b7e42cfabc1448c6a57d7e68fd5a49756
gavrushoves <gavrushoves@list.ru>
2017-07-09 17:13:14 +0300

    input
    математическая операция с пользовательским вводом.

---
+++ 2.js
@@ -0,0 +1,8 @@
+function pow2(){
+    //document.getElementById"('inp1').value
+    alert(document.getElementById('inp1').value*document.getElementById('inp1').value);
+}
+
+function pow3(){
+    alert(document.getElementById('inp1').value*document.getElementById('inp1').value*document.getElementById('inp1').value)
+}
