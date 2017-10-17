0f644b492b4fc5880a1d7c325a02352e1a409c86
gavrushoves <gavrushoves@list.ru>
2017-10-18 00:22:21 +0300

    Delete 2.js

--- 2.js
+++
@@ -1,8 +0,0 @@
-function pow2(){
-    //document.getElementById"('inp1').value
-    alert(document.getElementById('inp1').value*document.getElementById('inp1').value);
-}
-
-function pow3(){
-    alert(document.getElementById('inp1').value*document.getElementById('inp1').value*document.getElementById('inp1').value)
-}
